d411ee6c5acd0d3f457e90437b8d3b5c0f97280b
pornchanok <pornchanok.phua@mail.kmutt.ac.th>
2025-06-01 01:30:36 +0700

    logout

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -3,7 +3,7 @@ import { JsonResponse } from "../utils/JsonResponse.ts";
 import * as userModel from "../models/user.model.ts";
 import * as bcrypt from "bcrypt";
 import * as jwt from "hono/jwt";
-import { setCookie } from "hono/cookie";
+import { deleteCookie, setCookie } from "hono/cookie";
 
 type RegisterPayload = {
   username: string;
@@ -33,10 +33,7 @@ export const Register = async (c: Context) => {
 
     const newUser = await userModel.createUser(body.username, hashedPassword);
 
-    return c.json(
-      JsonResponse(true, "User created!", { id: newUser.id }),
-      201
-    );
+    return c.json(JsonResponse(true, "User created!", { id: newUser.id }), 201);
   } catch (e) {
     console.log(e);
 
@@ -45,36 +42,64 @@ export const Register = async (c: Context) => {
 };
 
 export const Login = async (c: Context) => {
-    try {
-        const body = await c.req.json<LoginPayload>();
+  try {
+    const body = await c.req.json<LoginPayload>();
 
-        if (!body.username || !body.password) return c.json(JsonResponse(false, "Missing Required field"), 400)
+    if (!body.username || !body.password)
+      return c.json(JsonResponse(false, "Missing Required field"), 400);
 
-        const user = await userModel.getUserByUsername(body.username);
+    const user = await userModel.getUserByUsername(body.username);
 
-        if (!user) return c.json(JsonResponse(false, "User not found"), 400)
+    if (!user) return c.json(JsonResponse(false, "User not found"), 400);
 
-        const isPasswordMatch = await bcrypt.compare(body.password, user.Password);
+    const isPasswordMatch = await bcrypt.compare(body.password, user.Password);
 
-        if (!isPasswordMatch) return c.json(JsonResponse(false, "Incorrect Password"), 400)
+    if (!isPasswordMatch)
+      return c.json(JsonResponse(false, "Incorrect Password"), 400);
 
-        const jwtPayload = {
-            id: user.id
-        }
+    const jwtPayload = {
+      id: user.id,
+    };
 
-        const SECRET = process.env.JWT_SECRET;
+    const SECRET = process.env.JWT_SECRET;
 
-        if (!SECRET) throw new Error("Missing JWT_SECRET in .env file")
+    if (!SECRET) throw new Error("Missing JWT_SECRET in .env file");
 
-        const token = await jwt.sign(jwtPayload, SECRET)
+    const token = await jwt.sign(jwtPayload, SECRET);
 
-        setCookie(c, "authToken", token)
+    setCookie(c, "authToken", token);
 
-        return c.json(JsonResponse(true, "Login Success!"), 200)
+    return c.json(JsonResponse(true, "Login Success!"), 200);
+  } catch (e) {
+    console.log(e);
 
-    } catch (e) {
-        console.log(e);
+    return c.json(JsonResponse(false, `Internal Server Error`, e), 500);
+  }
+};
 
-        return c.json(JsonResponse(false, `Internal Server Error`, e), 500)
-    }
-}
+export const Logout = async (c: Context) => {
+  try {
+    deleteCookie(c, "authToken", {
+      path: "/",
+      httpOnly: true,
+      secure: false,
+    });
+
+    return c.json(
+      {
+        success: true,
+        msg: "Logout Successful!",
+      },
+      200
+    );
+  } catch (e) {
+    return c.json(
+      {
+        success: false,
+        data: e,
+        msg: "Logout failed!!",
+      },
+      500
+    );
+  }
+};
