26fce558e1fef2c30b254b75dbd225214f09d19b
pornchanok <pornchanok.phua@mail.kmutt.ac.th>
2025-05-31 09:56:24 +0700

    set up frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,74 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Homepage from "./page/Homepage";
+import AddNewCategory from "./page/AddNewCategory";
+import CategoryItem from "./page/CategoryItem";
+import DirectSwipe from "./page/DirectSwipe";
+import EnterMember from "./page/EnterMember";
+import Login from "./page/Login";
+import Register from "./page/Register";
+import Match from "./page/Match";
+import Participants from "./page/Participants";
+import Profile from "./page/Profile";
+import Swipe from "./page/Swipe";
+import Waiting from "./page/Waiting";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: < Homepage />,
   },
-
+  {
+    path: "/home",
+    element: < Homepage />,
+  },
+  {
+    path: "/addnewcategory",
+    element: < AddNewCategory />,
+  },
+  {
+    path: "/categoryitem",
+    element: < CategoryItem />,
+  },
+  {
+    path: "/directswipe",
+    element: < DirectSwipe />,
+  },
+  {
+    path: "/entermember",
+    element: < EnterMember />,
+  },
+  {
+    path: "/login",
+    element: < Login />,
+  },
+  {
+    path: "/register",
+    element: < Register />,
+  },
+  {
+    path: "/match",
+    element: < Match />,
+  },
+  {
+    path: "/participants",
+    element: < Participants />,
+  },
+  {
+    path: "/profile",
+    element: < Profile />,
+  },
+  {
+    path: "/swipe",
+    element: < Swipe />,
+  },
+  {
+    path: "/waiting",
+    element: < Waiting />,
+  },
+  
 ]);
 
 export default function AppRouter() {
---
+++ frontend/src/page/Waiting.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Waiting() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Waiting
