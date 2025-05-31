ce92ff44aac2c70074fe29d7e775c7d2c8143858
Babyroof <meeraksatanapoom@gmail.com>
2025-05-31 09:25:26 +0700

    Initial Project

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { PrismaClient } from "./generated/prisma/index.js";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+export const db = new PrismaClient();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
